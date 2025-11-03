7c1b87f8e201f8b642a59c57ddf4ffa1f94e3162
WoutGrovenPXL <117352806+WoutGrovenPXL@users.noreply.github.com>
2025-11-03 09:50:36 +0100

    Lab 2 gemaakt

--- backend-java/SpringCloudPXL/department-service/src/test/java/be/pxl/services/DepartmentTests.java
+++ backend-java/SpringCloudPXL/department-service/src/test/java/be/pxl/services/DepartmentTests.java
@@ -1,7 +1,66 @@
 package be.pxl.services;
 
+import be.pxl.services.domain.Department;
+import be.pxl.services.repository.DepartmentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 public class DepartmentTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Container
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0");
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Test
+    public void testCreateDepartment() throws Exception {
+
+        Department department = Department.builder()
+                .name("IT")
+                .position("buh")
+                .build();
+
+        String departmentString = objectMapper.writeValueAsString(department);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/department")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(departmentString))
+                .andExpect(status().isCreated());
+
+
+        assertEquals(1, departmentRepository.findAll().size());
+    }
+
+
 }
